b5fb3e367703fdd76777b80f9e5ecb0dd9dd1a45
nicolecrmm <nicole_de-oliveira@estudante.sc.senai.br>
2022-10-06 09:24:14 -0300

    CERTO
    CERTO

--- cadastrar.php
+++ cadastrar.php
@@ -4,12 +4,12 @@ $nome = $_POST["nome"];
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
-$comando = $pdo->prepare("INSERT INTO cadastro(nome, email, senha) VALUES(:nome,:email,:senha) ");
+$comando = $pdo->prepare("INSERT INTO cliente   (nome, email, senha) VALUES(:nome,:email,:senha) ");
 
     $comando->bindValue(":nome",$nome);
     $comando->bindValue(":email",$email);
     $comando->bindValue(":senha",$senha);
    
     $comando->execute();
-    echo ("dados gravados");
+    header("location:procedimento.html");
 ?>
