3b4198f630abb2cb05168c79e99d57f3e9310b46
nicolecrmm <nicole_de-oliveira@estudante.sc.senai.br>
2022-11-09 12:30:21 -0300

    Update banco_cartao.php

--- banco_cartao.php
+++ banco_cartao.php
@@ -3,16 +3,18 @@ include ("config.php");
 session_start();
 $numero_cartao = $_POST["numero_cartao"];
 $data_validade = $_POST["data_validade"];
-$cvv = $_POST["cvv"];
+$cvv = $_POST["cvv"];;
 $nome_usuario_cartao =  $_POST["nome_usuario_cartao"]; 
+$id_cliente = $_SESSION["id_cliente"];
 
-$comando = $pdo->prepare("INSERT INTO pagamento   (numero_cartao, data_validade, cvv, nome_usuario_cartao) VALUES(:numero_cartao,:data_validade,:cvv, :nome_usuario_cartao) ");
+
+$comando = $pdo->prepare("INSERT INTO pagamento   (numero_cartao, data_validade, cvv, nome_usuario_cartao, id_cliente) VALUES(:numero_cartao,:data_validade,:cvv, :nome_usuario_cartao, :id_cliente) ");
 
     $comando->bindValue(":numero_cartao",$numero_cartao);
     $comando->bindValue(":data_validade",$data_validade);
     $comando->bindValue(":cvv",$cvv);
     $comando->bindValue(":nome_usuario_cartao",$nome_usuario_cartao);
-    
+    $comando->bindValue(":id_cliente",$id_cliente);
     $comando->execute();
     header("location:pag_feito.html");
 ?>
